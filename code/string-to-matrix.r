ace9ffdc5dcad9fd2b1fa8b2ede2d20793b2be30
Unknown <anton.collentine@gmail.com>
2018-04-22 00:31:54 +0200

    Added support for "X1 > 0, X2 > 0" type hypotheses

--- code/string-to-matrix.r
+++ code/string-to-matrix.r
@@ -24,7 +24,7 @@ grep("X", hyp) #returns the position instead of a logical
 grep("[a-zA-Z]+", step1) #check which subunit contains at least one letter https://stackoverflow.com/questions/3617797/regex-to-match-only-letters
 grepl("^[0-9a-zA-Z><=,]+$", hyp) #Here we look only for things contained in [], 'start with' (^) and 'end with' ($) anything instide [] any number of times (+) 
 if(all(input_vars %in% varnames)) #if requires one TRUE/FALSE, 'all' checks if all logicals are TRUE
-  gsub("([>=])","~\\1~",hyp) #surrounds the matched pattern by ~ which means the >= can be extracted in strsplit if desired
+gsub("([>=])","~\\1~",hyp) #surrounds the matched pattern by ~ which means the >= can be extracted in strsplit if desired
 gregexpr("[>=]", hyp) #gregexpr gives us the positions of all matches (regexpr of only first), nice to use wiht unlist()
 substring(hyp, pos) #By default extracts from the position (inclusive) until the end
 #+ #matches something one or more times
@@ -64,26 +64,30 @@ d <- sim_reg_data(c(0.2, 0.1, 0.3, 0.4, 0.1, 0))
 q <- lm(y ~ X1 + X2 + X3 + X4 + X5 + X6, data = d)
 object <- q
 
-hyp <- "X1 = X4 > (X3, X5, X2)"
+hyp <- "X1 = X2, (X4, X5) > X3, X6 < 2" 
+#Problem, can't handle this combination of parentheses and non-parentheses
+#With the parenthesis part surrounded by other comma
 
 create_matrices(q, hyp)
 #***************************************************
 #Function string-to-matrices----
 #***************************************************
 
-#Note: As opposed to the implemented function, this one does not permit specification of multiple hypotheses
+#Note: As opposed to the implemented Bayes Factor function, this one does not permit specification of multiple hypotheses
 #using semicolons. This is because upon implementation I loop over this function for each hypothesis.
+#The implemented Bayes Factor function does not collect the matrices in a list for output, but uses them directly
+
 
 create_matrices <- function(object, hyp){
   
   varnames <- variable.names(object) #provides the variable names of the linear model object, including intercept
   if(is.null(varnames)) stop("Please input proper linear model object")
   
-  hyp2 <- gsub("[ ()]", "", hyp) #removes all whitespace and parentheses
-  if(!grepl("^[0-9a-zA-Z><=,]+$", hyp2)) stop("Impermissable characters in hypotheses. Only letters, numbers and '> < = (),' permitted") #Self-explanatory
+  hyp2 <- gsub(" ", "", hyp) #removes all whitespace
+  if(!grepl("^[0-9a-zA-Z><=,()]+$", hyp2)) stop("Impermissable characters in hypotheses. Only letters, numbers and '> < = (),' permitted") #Self-explanatory. NEW parentehese
   if(grepl("[><=]{2,}", hyp2)) stop("Do not use combined comparison signs e.g., '>=' or '=='")
   
-  step1 <- unlist(strsplit(hyp2, split = "[<>=,]")) #split by comparison signs and unlist
+  step1 <- unlist(strsplit(hyp2, split = "[<>=,()]")) #split by special characters and unlist
   input_vars <- step1[grep("[a-zA-Z]+", step1)] #extract subunits that contain at least one letter
   if(!all(input_vars %in% varnames)) stop("Hypothesis variable(s) not in object, check spelling") #Checks if input variables exist in lm-object
   
@@ -101,26 +105,56 @@ create_matrices <- function(object, hyp){
   comparisons <- substring(hyp2, pos_comparisons, pos_comparisons) #Extract comparison signs
   framed <- data.frame(left = leftside, comp = comparisons, right = rightside, stringsAsFactors = FALSE) #hypotheses as a dataframe
   
-  commas <- unique(c(grep(",", framed$left), grep(",", framed$right))) #Gives us the unique rows that contain commas (multiple comparisons) from left or right columns
-  if(length(commas) > 0){ #If there are any multiple comparisons e.g., (X1, X2) separate these
-  multiples <- vector("list", length = length(commas)) #Empty vector to store results for each row in loop below
-  for(r in seq_along(commas)){ #for each row containing commas
-    several <- framed[commas,][r, ] #select row r
-    leftvars <- unlist(strsplit(several$left, split = ",")) #separate left hand var
-    rightvars <- unlist(strsplit(several$right, split = ",")) #separate right hand vars
-    if(any(grepl("^$", leftvars)) || any(grepl("^$", rightvars))) stop("Misplaced comma in hypothesis") #if empty element after strsplit
+  if(grepl(",", framed$left) || grepl(",", framed$right)){ #Larger loop that deals with commas if the specified hypothesis contains any
+    if(nrow(framed) > 1){
+      for(r in 1:(nrow(framed)-1)){ #If a hypothesis has been specified with commas e.g., "X1 > 0, X2 > 0" or "(X1, X2) > X3"
+        if(all.equal(framed$right[r], framed$left[r+1])){ #The right hand side of the hypothesis df will be equal to the next row left side
+          if(substring(framed$right[r], 1, 1) == "(") { #If the first row begins with a ( as when "X1 > (X2, X3)" and opposed to "(X2, X3) > X1"
+            framed$right[r] <- sub("),.+", ")", framed$right[r])#If so, remove everything to the right of the parenthesis on the right hand side
+            framed$left[r+1] <- sub(".+),", "", framed$left[r +1])#and everything to the left of the parenthesis on the left hand side to correct the df
+            } else{
+              framed$right[r] <- sub(",.+", "", framed$right[r]) #else, remove everything to the right of the comma on the right hand side
+              framed$left[r+1] <- sub("[^,]+,", "", framed$left[r+1]) #and everything to the left of the comma on the left hand side to correct the df
+            }
+          }
+        }
+      } 
     
-    left <- rep(leftvars, each = length(rightvars)) #repeat each leftvars the number of rightvars
-    right <- rep(rightvars, each = length(leftvars)) #complement for rightvars
-    comp <- rep(several$comp, length(left)) #repeat the comparison a corresponding number of times
+    commas_left <- framed$left[grep(",", framed$left)] #At this point all remaining elements that contain commas should also have parentheses, check this
+    commas_right <- framed$right[grep(",", framed$right)] #Necessary to use is isTRUE below in case one of these contains no commas, and 'any' for several rows
+    if(isTRUE(any(!grepl("\\(.+)", commas_left))) || isTRUE(any(!grepl("\\(.+)", commas_right))) || #Check so rows contain parenthesis
+       isTRUE(any(grepl(").+", commas_left))) || isTRUE(any(grepl(").+", commas_right))) || #Check so parentheses are not followed by anything
+       isTRUE(any(grepl(".+\\(", commas_left))) || isTRUE(any(grepl(".+\\(", commas_right)))) { #chekc so parentheses are not preceded by anything
+      stop("Incorrect hypothesis syntax or extra character, check specification")
+      }
     
-    multiples[[r]] <- data.frame(left = left, comp = comp, right = right, stringsAsFactors = FALSE) #save as df to be able to combine with 'framed'
-  }
-  
-  framed <- framed[-commas,] #remove old unfixed rows with commas
-  multiples <- do.call(rbind, multiples) #make list into dataframe
-  framed <- rbind(multiples, framed) #recombine into one dataframe
-  }
+    
+    framed$left <- gsub("[()]", "", framed$left) #drop remaining parentheses
+    framed$right <- gsub("[()]", "", framed$right)
+    commas <- unique(c(grep(",", framed$left), grep(",", framed$right))) #Gives us the unique rows that still contain commas (multiple comparisons) from left or right columns
+
+    if(length(commas) > 0){ #If there are any multiple comparisons e.g., (X1, X2) below loop separates these in
+      multiples <- vector("list", length = length(commas)) #Empty vector to store results for each row in loop below
+
+      for(r in seq_along(commas)){ #for each row containing commas
+        several <- framed[commas,][r, ] #select row r
+    
+        leftvars <- unlist(strsplit(several$left, split = ",")) #separate left hand var
+        rightvars <- unlist(strsplit(several$right, split = ",")) #separate right hand vars
+        if(any(grepl("^$", leftvars)) || any(grepl("^$", rightvars))) stop("Misplaced comma in hypothesis") #if empty element after strsplit
+    
+        left <- rep(leftvars, length.out = length(rightvars)*length(leftvars)) #repeat each leftvars the number of rightvars
+        right <- rep(rightvars, each = length(leftvars)) #complement for rightvars
+        comp <- rep(several$comp, length(left)) #repeat the comparison a corresponding number of times
+    
+        multiples[[r]] <- data.frame(left = left, comp = comp, right = right, stringsAsFactors = FALSE) #save as df to be able to combine with 'framed'
+      }
+
+      framed <- framed[-commas,] #remove old unfixed rows with commas
+      multiples <- do.call(rbind, multiples) #make list into dataframe
+      framed <- rbind(multiples, framed) #recombine into one dataframe
+    }
+  } #end comma loop
   
   equality <- framed[framed$comp == "=",]
   inequality <- framed[!framed$comp == "=",]
@@ -130,13 +164,13 @@ create_matrices <- function(object, hyp){
     list_equality <- NULL
   } else{
     outcomes <- suppressWarnings(apply(equality[, -2], 2, as.numeric)) #Convert left/right to numeric, non-numeric values (variables) coerced to NA 
-    outcomes <- matrix(outcomes, ncol = 2, byrow = TRUE) #Conversion to matrix in case there was only one row in outcomes
+    outcomes <- matrix(outcomes, ncol = 2, byrow = FALSE) #Conversion to matrix in case there was only one row in outcomes
     if(any(rowSums(is.na(outcomes)) == 0)) stop("Value compared with value rather than variable, e.g., '2 = 2', check hypotheses")
-    cols <- which(rowSums(is.na(outcomes)) < 2) #which columns contain a numeric value (comparing variable to value), that is not two NA-values
-    specified <- t(outcomes[cols,]) #transpose so that specified comparison values are extracted in correct order below
+    rows <- which(rowSums(is.na(outcomes)) < 2) #which rows contain a numeric value (comparing variable to value), that is not two NA-values
+    specified <- t(outcomes[rows,]) #transpose so that specified comparison values are extracted in correct order below, e.g, in case when "X1 = 0, 2 = X2"
     specified <- specified[!is.na(specified)] #extract specified comparison values
     r_e <- ifelse(rowSums(is.na(outcomes)) == 2, 0, specified) #If variable = variable -> 0, if variable = value -> value
-    r_e <- matrix(r_e, ncol = length(r_e)) #convert to matrix
+    r_e <- matrix(r_e) #convert to matrix
     
     var_locations <- apply(equality[, -2], 2, function(x) ifelse(x %in% varnames, match(x, varnames), 0)) #convert non-variables to 0 and others are given their locations in varnames
     var_locations <- matrix(var_locations, ncol = 2) #Necessary if only one comparison row
@@ -158,13 +192,13 @@ create_matrices <- function(object, hyp){
     list_inequality <- NULL 
   } else{
     outcomes <- suppressWarnings(apply(inequality[, -2], 2, as.numeric)) #Convert left/right to numeric, non-numeric values (variables) coerced to NA 
-    outcomes <- matrix(outcomes, ncol = 2, byrow = TRUE) #Conversion to matrix in case there was only one row in outcomes
+    outcomes <- matrix(outcomes, ncol = 2, byrow = FALSE) #Conversion to matrix in case there was only one row in outcomes
     if(any(rowSums(is.na(outcomes)) == 0)) stop("Value compared with value rather than variable, e.g., '2 > 2', check hypotheses")
     cols <- which(rowSums(is.na(outcomes)) < 2) #which columns contain a numeric value (comparing variable to value), that is not two NA-values
     specified <- t(outcomes[cols,]) #transpose so that specified comparison values are extracted in correct order below
     specified <- specified[!is.na(specified)] #extract specified comparison values
     r_i <- ifelse(rowSums(is.na(outcomes)) == 2, 0, specified) #If variable = variable -> 0, if variable = value -> value
-    r_i <- matrix(r_i, ncol = length(r_i)) #convert to matrix
+    r_i <- matrix(r_i) #convert to matrix
     
     leq <- which(inequality$comp == "<") #gives the rows that contain '<' (lesser or equal) comparisons
     var_locations <- apply(inequality[, -2], 2, function(x) ifelse(x %in% varnames, match(x, varnames), 0)) #convert non-variables to 0 and others are given their locations
